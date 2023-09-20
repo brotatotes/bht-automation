533c12a03c685f95a95d19e12629597d4655f958
Eric Hao <erichao2018@gmail.com>
2023-09-20 19:30:01 -0400

    analyze injected words

--- check_choicests.py
+++ check_choicests.py
@@ -1,6 +1,8 @@
 
 import os
 import re
+import string
+from gensim.utils import tokenize
 
 COMMENTATORS = set([
     "Henry Alford",
@@ -34,9 +36,13 @@ def get_commentary(commentator, book, chapter, verse):
 
 folder = "bht gen 1"
 
-count = 0
+verse_count = 0
+missing_commentary_quotes_count = 0
+corrupted_commentary_quotes_count = 0
+corrupted_verses_count = 0
 
-output_file = open('check_choicests.txt', 'w')
+output_file = open('check_choicests.md', 'w')
+output_file.write("# Issues Found:\n\n")
 
 for book in os.listdir(folder):
     if book.startswith('.'):
@@ -47,6 +53,8 @@ for book in os.listdir(folder):
             continue
 
         for verse in os.listdir(f"{folder}/{book}/{chapter}"):
+            verse_count += 1
+            print(f"\r{verse_count} / 7957", end="", flush=True)
             if verse.startswith('.'):
                 continue
 
@@ -66,8 +74,66 @@ for book in os.listdir(folder):
                 no_commentary = not commentary_content
 
                 if not no_commentary:
-                    output_file.write(f"{missing_commentator} for {book} {chapter_number}:{verse_number} actually has commentary!\n")
-                    count += 1
+                    output_file.write(f"## {missing_commentator} for {book} {chapter_number}:{verse_number} actually has commentary!\n\n")
+                    missing_commentary_quotes_count += 1
 
-output_file.write(str(count))
+            # parse bht_content into commentator quotes.
+            current_commentator = None
+            commentator_quotes = {}
+            for line in bht_content.splitlines():
+                if line.startswith("###"):
+                    current_commentator = line[4:-1]
+                    continue
+                elif line.startswith("#"):
+                    current_commentator = None
+
+                if not current_commentator:
+                    continue
+
+                if not current_commentator in commentator_quotes:
+                    commentator_quotes[current_commentator] = []
+
+                commentator_quotes[current_commentator].append(line)
+
+            # print(commentator_quotes)
+
+            corrupted_verse = False
+
+            # verify that all commentary quotes really come from commentary.
+            for commentator in commentators:
+                commentary = get_commentary(commentator, book, chapter_number, verse_number)
+                commentary_tokens = set(tokenize(commentary.lower()))
+                # print(f"Commentator:\n{commentator}")
+                # print()
+                # print(f"Commentary Tokens:\n{commentary_tokens}")
+                # print()
+
+                for quote in commentator_quotes[commentator]:
+                    quote_tokens = set(tokenize(quote.lower()))
+
+                    # print(f"Quote Tokens:\n{quote_tokens}")
+                    # print()
+                    # print(f"all quote words found in commentary: {len(quote_tokens - commentary_tokens) == 0}")
+                    # print(f"{quote_tokens - commentary_tokens}")
+                    # input()
+
+                    tokens_added_by_gpt = quote_tokens - commentary_tokens
+                    if len(tokens_added_by_gpt) > 0:
+                        output_file.write(f"## {book} {chapter_number} {verse_number} {commentator} corrupted.\n") 
+                        output_file.write(f"Quote: [{quote}]\n")
+                        output_file.write(f"Added words: [{tokens_added_by_gpt}]\n\n")
+                        corrupted_commentary_quotes_count += 1
+                        corrupted_verse = True
+                        break
+
+            if corrupted_verse:
+                corrupted_verses_count += 1
+
+
+
+
+output_file.write("\n# Summary:\n")
+output_file.write(f"Missing Commentary Quotes Count: {missing_commentary_quotes_count} (How many commentaries should have quotes but they're missing?)\n")
+output_file.write(f"Corrupted Verses Count: {corrupted_verses_count} (How many verses have quotes with chatGPT injected opinions?)\n")
+output_file.write(f"Corrupted Commentary Quotes Count: {corrupted_commentary_quotes_count} (How many commentaries have quotes with chatGPT injected opinions?)\n")
 output_file.close()
